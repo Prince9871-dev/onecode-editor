a08e9dc0c7e9e431f70f1f4a6d2b6f65571b8c24
Prince9871-dev <princejha2003@gmail.com>
2026-04-24 23:46:51 +0530

    final middleware production fix

--- middleware.ts
+++ middleware.ts
@@ -8,43 +8,58 @@ import {
 } from "@/routes";
 import authConfig from "./auth.config";
 
-
 const { auth } = NextAuth(authConfig);
 
 // @ts-ignore
 export default auth((req) => {
   const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
+
   const host = req.headers.get("host");
+
   const requestOrigin = host
     ? `${nextUrl.protocol}//${host}`
     : nextUrl.origin;
 
-  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isApiAuthRoute =
+    nextUrl.pathname.startsWith(apiAuthPrefix);
 
-  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isPublicRoute =
+    publicRoutes.includes(nextUrl.pathname);
 
-  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  const isAuthRoute =
+    authRoutes.includes(nextUrl.pathname);
 
+  // Allow NextAuth API routes
   if (isApiAuthRoute) {
     return null;
   }
 
+  // If user visits auth pages like sign-in/sign-up
   if (isAuthRoute) {
     if (isLoggedIn) {
-      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, requestOrigin));
+      return Response.redirect(
+        new URL(DEFAULT_LOGIN_REDIRECT, requestOrigin)
+      );
     }
+
     return null;
   }
 
-  if(!isLoggedIn && !isPublicRoute){
-    return Response.redirect(new URL("/auth/sign-in" , requestOrigin))
+  // Protect private routes
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(
+      new URL("/auth/sign-in", requestOrigin)
+    );
   }
 
-  return null
+  return null;
 });
 
 export const config = {
-  // copied from clerk
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    "/((?!.+\\.[\\w]+$|_next).*)",
+    "/",
+    "/(api|trpc)(.*)",
+  ],
 };
